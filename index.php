67e359624082343d61b191f2201bb60f7befd264
Vadim I. <vadign00@gmail.com>
2024-08-26 02:50:05 +0200

    Minor cleanup

--- index.php
+++ index.php
@@ -17,15 +17,15 @@ $csv = array_map('str_getcsv', file($path.'.csv'));
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/css/common.css">
-<link rel="stylesheet" href="css/main.css?4">
+<link rel="stylesheet" href="css/main.css?v=1">
 <script src="libs/jquery-3.7.1.min.js"></script>
-<script src="libs/turn.min.js?1"></script>
-<script src="js/main.js?6"></script>
+<script src="libs/turn.min.js?v=1"></script>
+<script src="js/main.js?v=1"></script>
 
 <div class="toolbar">
   <div id="arrows-left" class="arrow-group disabled">
-    <img class="arrow flipped" id="arrow-first" src="img/arrow_end.svg?" alt>
-    <img class="arrow flipped" id="arrow-prev" src="img/arrow.svg?" alt>
+    <img class="arrow flipped" id="arrow-first" src="img/arrow_end.svg" alt>
+    <img class="arrow flipped" id="arrow-prev" src="img/arrow.svg" alt>
   </div>
   <form class="toolbar-page-selector" id="page-selector">
     <input class="toolbar-field toolbar-page-number" id="page-input" type="number" autocomplete="off" min="1">
@@ -33,8 +33,8 @@ $csv = array_map('str_getcsv', file($path.'.csv'));
     <div>из <span id="total-pages"></span></div>
   </form>
   <div id="arrows-right" class="arrow-group">
-    <img class="arrow" id="arrow-next" src="img/arrow.svg?" alt>
-    <img class="arrow" id="arrow-last" src="img/arrow_end.svg?" alt>
+    <img class="arrow" id="arrow-next" src="img/arrow.svg" alt>
+    <img class="arrow" id="arrow-last" src="img/arrow_end.svg" alt>
   </div>
 </div>
 <main class="flipbook-wrapper">
@@ -44,7 +44,7 @@ $csv = array_map('str_getcsv', file($path.'.csv'));
 </main>
 <script src="libs/telegram-web-app.min.js"></script>
 <script>
-  Telegram.WebApp.ready();
-  Telegram.WebApp.expand();
-  Telegram.WebApp.BackButton.show().onClick(Telegram.WebApp.close);
+Telegram.WebApp.ready();
+Telegram.WebApp.expand();
+Telegram.WebApp.BackButton.show().onClick(Telegram.WebApp.close);
 </script>
